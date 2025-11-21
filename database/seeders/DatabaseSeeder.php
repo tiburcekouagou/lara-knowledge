9c3c8e81f3429e24cc79595bec1dfd60d2dce24c
Tiburce Kouagou <kouagoutiburce@gmail.com>
2025-11-21 17:31:48 +0100

    feat: auth and roles setup
    - Install Laravel Breeze
    - Add avatar column to users table
    - Install Spatie Permission package
    - Configure User model with HasRoles
    - Seed database with Admin and Users roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // 1. Création des Rôles
+        $adminRole = Role::create(['name' => 'admin']);
+        $userRole = Role::create(['name' => 'user']);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 2. Création de l'administrateur
+        $admin = User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@highfive.com',
+            'password' => bcrypt('password'),
         ]);
+        $admin->assignRole($adminRole); // Attribution du rôle d'administrateur
+
+        // 3. Création d'un utilisateur standard pour tester
+        $user = User::factory()->create([
+            'name' => 'Alice Doe',
+            'email' => 'alice@highfive.com',
+            'password' => bcrypt('password'),
+        ]);
+        $user->assignRole($userRole); // Attribution du rôle d'utilisateur
     }
 }

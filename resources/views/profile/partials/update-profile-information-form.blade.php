676bb32466cfa70dacff2a9985f9a57242448f74
Tiburce Kouagou <kouagoutiburce@gmail.com>
2025-11-24 11:47:39 +0100

    feat: user avatar upload - Link public storage directory - Update ProfileUpdateRequest to validate avatar image - Implement avatar upload and cleanup logic in ProfileController - Add file input and image priview in profile edit view

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,7 +13,7 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
         @csrf
         @method('patch')
 
@@ -46,6 +46,21 @@
                 </div>
             @endif
         </div>
+
+        <!-- Prévisualisation Avatar -->
+        @if ($user->avatar)
+            <div class="mt-2">
+                <img src="{{ asset('storage/' . $user->avatar) }}" alt="{{ 'Profil de ' . $user->name }}" class="w-20 h-20 rounded-full object-cover" />
+            </div>
+        @endif
+        <!--/ Prévisualisation Avatar -->
+        <!-- Avatar Upload -->
+        <div>
+            <x-input-label for="avatar" :value="__('Avatar')" />
+            <input id="avatar" name="avatar" type="file" class="mt-1 block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" />
+            <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
+        </div>
+        <!--/ Avatar Upload -->
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,7 +15,29 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile', function (ProfileUpdateRequest $request) {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        // Traitement de l'image de profil
+        if ($request->hasFile('avatar')) {
+            // 1. Supprimer l'ancienne image si elle existe
+            if ($request->user()->avatar) {
+                Storage::disk('public')->delete($request->user()->getOriginal('avatar'));
+            }
+            // 2. Stocker la nouvelle image
+            $path = $request->file('avatar')->store('avatars', 'public');
+            // 3. Sauvagarder le chemin de l'image dans la base de données
+            $request->user()->avatar = $path;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+    })->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 

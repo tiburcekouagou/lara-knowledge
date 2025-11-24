39f387f534655f89177b42cc6838c207d4b1a881
Tiburce Kouagou <kouagoutiburce@gmail.com>
2025-11-24 17:57:47 +0100

    feat: implement article update and delete authorization logic

--- app/Policies/ArticlePolicy.php
+++ app/Policies/ArticlePolicy.php
@@ -37,7 +37,7 @@ class ArticlePolicy
      */
     public function update(User $user, Article $article): bool
     {
-        return true;
+        return $user->id === $article->user_id || $user->hasRole('admin');
     }
 
     /**
@@ -45,7 +45,8 @@ class ArticlePolicy
      */
     public function delete(User $user, Article $article): bool
     {
-        return true;
+        return $user->id === $article->user_id || $user->hasRole('admin');
+
     }
 
     /**
